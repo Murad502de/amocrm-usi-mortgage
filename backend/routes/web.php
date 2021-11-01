600904fcfc27ec6bc3851cfa0ffca2f92b2ee58a
Мурад Расулов <rassulovmurad@gmail.com>
2021-11-01 20:27:14 +0300

    test

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,22 +1,7 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+$router->get( '/api/auth', 'Api\Services\amoAuthController@auth' );
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-use App\Http\Controllers\Api\Services\amoAuthController;
-
-$router->get( '/api/auth', 'amoAuthController@auth' );
-
-$router->get('/', function () use ($router) {
+$router->get( '/', function () use ( $router ) {
     return 'test commit hallo';
-});
+} );
